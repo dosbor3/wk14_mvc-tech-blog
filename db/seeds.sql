39bc50635a35f79654f96e29e997ad43412a36fa
I am Billie <dosbor3@gmail.com>
2022-03-06 09:27:22 -0600

    homepage-view, main.hbs, and home-routes.js updated, seeding for users complete

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO user (username, email, password)
+VALUES
+  ('rfirbanks23', 'rfirbanks23@aol.com','password1'),
+  ('vwoolf87', 'vwoolf87@yahoo.com', 'oerjkd98'),
+  ('pgaveston30', 'pgaveston30@yahoo.com', 'dkeiona38k'),
+  ('cleroi7744', 'cleroi7744@gmail.com', 'kdfj9idofj'),
+  ('kmansfield3321', 'kmansfield3321@srg.org', 'eikkllke1122'),
+  ('dcarrington1504', 'dcarrington1504@summit.com', 'password3'),
+  ('ebellamy57', 'ebellamy57@aol.com', 'password2')  
