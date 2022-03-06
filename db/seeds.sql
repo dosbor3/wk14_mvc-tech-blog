f2004765af5e93cdc9c14a34e25e57eca8e541f9
I am Billie <dosbor3@gmail.com>
2022-03-06 13:51:18 -0600

    single-post feature complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,4 +6,27 @@ VALUES
   ('cleroi7744', 'cleroi7744@gmail.com', 'kdfj9idofj'),
   ('kmansfield3321', 'kmansfield3321@srg.org', 'eikkllke1122'),
   ('dcarrington1504', 'dcarrington1504@summit.com', 'password3'),
-  ('ebellamy57', 'ebellamy57@aol.com', 'password2')  
+  ('ebellamy57', 'ebellamy57@aol.com', 'password2'); 
+
+  -- INSERT INTO post (created_at, updated_at, title, post_url, user_id)
+  -- VALUES
+  -- ("3301", "4402", 'Taskmaster goes public!', 'https://taskmaster.com/press', 1 ),
+  -- ("3301", "4402"'School of Science Technology!', 'https://www.indeed.com/', 2 ),
+  -- ("3301", "4402"'Technology!', 'https://www.nytimes.com/section/technology', 3 ),
+  -- ("3301", "4402",'Russia Blocks Facebook!', 'https://www.nytimes.com/2022/03/04/world/europe/russia-facebook-ukraine.html', 3 ),
+  -- ("3301", "4402",'Object-Relational Mapping!', 'https://en.wikipedia.org/wiki/Object%E2%80%93relational_mapping', 4 ),
+  -- ("3301", "4402",'What is ORM and Why You Should Use it!', 'https://blog.bitsrc.io/what-is-an-orm-and-why-you-should-use-it-b2b6f75f5e2a', 5 ),
+  -- ("3301", "4402",'Comparing ORM vs SQL!', 'https://axiomq.com/blog/comparing-orm-vs-sql-what-to-know/', 6 ),
+  -- ("3301", "4402",'What are the best Object Relational Mapper (ORM) Tools?!', 'https://taskmaster.com/press', 1 ),
+  -- ("3301", "4402",'Taskmaster goes public!', 'https://stackshare.io/orm', 6 ),
+  -- ("3301", "4402",'Taskmaster goes public!', 'https://taskmaster.com/press', 4 ),
+  -- (3301, 4402,'Taskmaster goes public!', 'https://taskmaster.com/press', 5 ),
+  -- (3301, 4402,'Taskmaster goes public!', 'https://taskmaster.com/press', 1 ),
+  -- (3301, 4402,'Taskmaster goes public!', 'https://taskmaster.com/press', 4 ),
+  -- (3301, 4402,'Taskmaster goes public!', 'https://taskmaster.com/press', 2 ),
+  -- (3301, 4402,'Taskmaster goes public!', 'https://taskmaster.com/press', 2 );
+  
+
+
+  
+  
