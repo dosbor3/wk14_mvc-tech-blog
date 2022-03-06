21a8acd9400dcb402a9d74922253aa799b5bfcf3
I am Billie <dosbor3@gmail.com>
2022-03-05 18:34:50 -0600

    Vote model and routes completed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); //creates a router instance that will gather all of the model 
                                             //routes and package and export them out
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/post', postRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('/Vote');
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
